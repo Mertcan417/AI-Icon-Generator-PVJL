cde885a8f1d00b06781277e3ad4d3de5eaf74ecb
mert <boymerti@gmail.com>
2024-06-18 02:46:48 +0200

    fixed issue with credentials database

--- src/pages/api/signup.js
+++ src/pages/api/signup.js
@@ -28,6 +28,7 @@ export default async function handler(req, res) {
 
       return res.status(200).json({ message: 'User signed up successfully', user: result[0] });
     } catch (error) {
+      console.log("THE ERROR:   "+ error);
       console.error('Error inserting user:', error);
       return res.status(500).json({ error: 'Internal server error' });
     }
--- src/pages/utils/db.js
+++ src/pages/utils/db.js
@@ -2,11 +2,11 @@
 import { Pool } from 'pg';
 
 const pool = new Pool({
-  user: `${process.env.USER}`,
-  host: `${process.env.HOST}`,  // Ensure this is the correct host
-  database: `${process.env.DATABASE}`,
-  password: `${process.env.PASSWORD}`,
-  port: `${process.env.PORT}`,
+  user: process.env.USER,
+  host: process.env.HOST,  // Ensure this is the correct host
+  database: process.env.DATABASE,
+  password: process.env.PASSWORD,
+  port: process.env.DB_PORT,
 });
 
 export async function query(text, params) {
