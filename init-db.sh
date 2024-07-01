2e4aa87f2aaa0a783446044b4c872013e06696dc
mert <boymerti@gmail.com>
2024-07-01 22:42:48 +0200

    add stuff

--- init-db.sh
+++ init-db.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
 set -e
 
+# Create the database
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" <<-EOSQL
     CREATE DATABASE "image-generator";
+EOSQL
+
+# Connect to the newly created database and create the users table
+psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "image-generator" <<-EOSQL
+    CREATE TABLE users (
+        id SERIAL PRIMARY KEY,
+        username VARCHAR(50) UNIQUE NOT NULL,
+        password VARCHAR(255) NOT NULL
+    );
 EOSQL
